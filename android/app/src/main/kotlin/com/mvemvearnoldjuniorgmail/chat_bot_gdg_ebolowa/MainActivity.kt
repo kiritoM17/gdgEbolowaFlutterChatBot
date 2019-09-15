21e9ed05add9338e6e5a0076cff45eb9a61f858a
kiritoM17 <mvemvearnoldjunior@gmail.com>
2019-09-15 10:53:07 +0100

    chat bot v1

---
+++ android/app/src/main/kotlin/com/mvemvearnoldjuniorgmail/chat_bot_gdg_ebolowa/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mvemvearnoldjuniorgmail.chat_bot_gdg_ebolowa
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
